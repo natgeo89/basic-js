c5b69e1255cc3d6af42560e7b79737e34dc2e62f
Sakharuk Viktar <natgeogr93@gmail.com>
2020-10-04 18:52:27 +0300

    feat: extenden-repeater task done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (typeof str !=='string') str = String(str);
+        
+  let {repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|'} = options;
+
+  let additionResultString = '';
+
+  if (typeof addition !== 'string') addition = String(addition);
+
+  if ( additionRepeatTimes !== 1 ){    
+    for ( let i = 1; i <= additionRepeatTimes; i++ ) {
+      additionResultString += `${additionSeparator}${addition}`;
+    }
+    additionResultString = `${additionResultString.slice(additionSeparator.length)}`;            
+  }
+
+  else{
+    additionResultString = addition;
+  }   
+
+  let resultString = '';
+
+  if ( repeatTimes !==1 ) {
+    for (let i = 1; i <= repeatTimes; i++){
+      resultString += `${separator}${str}${additionResultString}`;        
+    }
+    resultString = `${resultString.slice(separator.length)}`;
+  }
+
+  else{
+    resultString = `${str}${additionResultString}`;
+  } 
+
+  return resultString;  
 };
+
+
   
