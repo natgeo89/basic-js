9f6eb83b96378eee418c5db2bc0f50f37e911285
Sakharuk Viktar <natgeogr93@gmail.com>
2020-10-04 18:51:29 +0300

    fix: fix argument adequate value validation

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,13 +4,13 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (!sampleActivity || typeof sampleActivity !== 'string') return false;
+  if (!sampleActivity || typeof sampleActivity !== 'string' || +sampleActivity <= 0 || +sampleActivity >= 8) return false;
 
   const k = 0.693 / HALF_LIFE_PERIOD;
 
   const time = Math.ceil((MODERN_ACTIVITY / +sampleActivity) / k);
   
-  if (!isFinite(time) || time < 22392) return false;
+  if (!isFinite(time)) return false;
 
   return time;
 
